2d50f6a9859c9ccd7cc5b940e4a549ee23eb3c56
Terry Crook <tcrook@q.com>
2014-10-08 07:06:14 -0600

    Continuing development
    Added support for CSV output, including macros for testing verbosity
    mask and printing CSV headings and values, plus definitions for Message
    Mask (verbosity) bits.

--- CommonDefs.h
+++ CommonDefs.h
@@ -11,7 +11,66 @@ Subsumption Architecture as described by David P. Anderson.
 #pragma once
 
 #include "Arduino.h"
+//#include <EEPROM.h>
 
 /// Print the value of the given variable, using the variable name as a label.
 /// Uses the F() macro to print strings directly from Program Memory without using RAM
 #define PRINT_VAR( x ) Serial.print( F( #x " = " ) ); Serial.println( x );
+
+#define USE_CSV
+#ifdef USE_CSV
+
+/// Determine whether to output CSV headings or data
+#define IF_CSV( MASK ) if ( _messageMask & MASK && pControlParams->PrintingCsv() )
+
+#define IF_MSG( MASK ) if ( _messageMask & MASK )
+
+/// Output either heading or data, separated by commas (or other delimiter)
+#define CSV_OUT( VAR ) \
+    if ( pControlParams->PrintingCsvHeadings() ) { \
+        Serial.print( _pName ); \
+        Serial.print( ':' ); \
+        Serial.print( F( #VAR ) ); \
+    } \
+    else \
+        Serial.print( VAR ); \
+    Serial.print( pControlParams->GetCsvDelimiter() );
+#else
+#define IF_CSV
+#define IF_MSG
+#define CSV_OUT
+#endif
+
+/// Message Mask bits
+/// Serial informational and diagnostic output is controlled by
+/// bitmapped variables in each class.  In Actors, this is controlled
+/// by the *V command.  For example, to enable all messages in the LED
+/// class, issue the command "LV 31"
+#define MM_RESPONSES    0x01
+#define MM_ID           0x02
+#define MM_PROGRESS     0x04
+#define MM_CALC         0x08
+#define MM_INFO         0x10
+#define MM_CSVBASIC     0x20
+#define MM_CSVEXTENDED  0x40
+
+
+//template <class T> int EEPROM_writeAnything(int ee, const T& value)
+//{
+//  const byte* p = (const byte*)(const void*)&value;
+//  unsigned int i;
+//  for (i = 0; i < sizeof(value); i++)
+//    EEPROM.write(ee++, *p++);
+//  return i;
+//}
+//
+//template <class T> int EEPROM_readAnything(int ee, T& value)
+//{
+//  byte* p = (byte*)(void*)&value;
+//  unsigned int i;
+//  for (i = 0; i < sizeof(value); i++)
+//    *p++ = EEPROM.read(ee++);
+//  return i;
+//}
+
+
